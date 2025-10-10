e7a43cd85a468372b232dedbd49809b9e3049adb
zjhuang24 <zjhuang24@gmail.com>
2025-10-10 12:32:01 -0500

    title style and format

--- flutter_app/lib/pages/home_page.dart
+++ flutter_app/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'route_page.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,25 +7,37 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Bus Route Optimizer'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    debugPrint('Get Started tapped');
-                    Navigator.pushNamed(context, RouteOptimization.routeName);
-                  },
-                  child: const Text("Get Started!"),
+        child: Padding(
+          padding: EdgeInsets.only(top: screenHeight * 0.15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Bus Route Optimizer',
+                style: GoogleFonts.oswald(
+                  fontSize: 80,
+                  fontWeight: FontWeight.w700,
                 ),
-              ],
-            ),
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      // debugPrint('Get Started tapped');
+                      Navigator.pushNamed(context, RouteOptimization.routeName);
+                    },
+                    child: Text("Get Started!"),
+                  ),
+                ],
+              ),
+              SizedBox(),
+            ],
+          ),
         ),
       ),
     );
