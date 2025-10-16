892364243a6341f40ad71abb1fab40cbe50f42d8
Zachary Huang <126215122+zjhuang24@users.noreply.github.com>
2025-10-16 12:52:57 -0500

    Flutter week02 (#2)
    * title style and format
    
    * home page background color and button format
    
    * firebase connection, email/password signup and login in forms
    
    * google auth for login

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/forms/login_form.dart';
+import 'package:flutter_app/forms/signup_form.dart';
 import 'pages/home_page.dart';
 import 'pages/route_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Core idea: prepare bindings for async init
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
 
 ///A StatefulWidget that represents app's home route
@@ -13,8 +19,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const HomePage(),
-      routes: {RouteOptimization.routeName: (_) => const RouteOptimization()},
+      routes: {
+        RouteOptimization.routeName: (_) => const RouteOptimization(),
+        SignupForm.routeName: (_) => const SignupForm(),
+        LoginForm.routeName: (_) => const LoginForm(),
+      },
     );
   }
 }
--- flutter_app/lib/pages/home_page.dart
+++ flutter_app/lib/pages/home_page.dart
@@ -1,30 +1,107 @@
 import 'package:flutter/material.dart';
-import 'route_page.dart';
+import 'package:flutter_app/forms/login_form.dart';
+import 'package:flutter_app/forms/signup_form.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
+      backgroundColor: Color.fromRGBO(57, 103, 136, 1),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Bus Route Optimizer'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    debugPrint('Get Started tapped');
-                    Navigator.pushNamed(context, RouteOptimization.routeName);
-                  },
-                  child: const Text("Get Started!"),
+        child: Padding(
+          padding: EdgeInsets.only(top: screenHeight * 0.15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Bus Route Optimizer',
+                style: GoogleFonts.quicksand(
+                  fontSize: 80,
+                  fontWeight: FontWeight.w700,
+                  color: const Color.fromARGB(255, 255, 255, 255),
                 ),
-              ],
-            ),
-          ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(padding: EdgeInsets.only(top: screenHeight * 0.05)),
+                  Text(
+                    "Sign up to get started, or log in if you already have an account.",
+                    style: GoogleFonts.quicksand(
+                      fontSize: 20,
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: TextButton(
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll<Color>(
+                              const Color.fromARGB(117, 255, 255, 255),
+                            ),
+                            padding: WidgetStatePropertyAll<EdgeInsets>(
+                              EdgeInsets.symmetric(
+                                horizontal: screenWidth * .01,
+                                vertical: screenHeight * .01,
+                              ),
+                            ),
+                          ),
+                          onPressed: () {
+                            // debugPrint('Get Started tapped');
+                            Navigator.pushNamed(context, SignupForm.routeName);
+                          },
+                          child: Text(
+                            "Sign up",
+                            style: GoogleFonts.quicksand(
+                              fontSize: 25,
+                              color: const Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: TextButton(
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll<Color>(
+                              const Color.fromARGB(117, 255, 255, 255),
+                            ),
+                            padding: WidgetStatePropertyAll<EdgeInsets>(
+                              EdgeInsets.symmetric(
+                                horizontal: screenWidth * .01,
+                                vertical: screenHeight * .01,
+                              ),
+                            ),
+                          ),
+                          onPressed: () {
+                            // debugPrint('Get Started tapped');
+                            Navigator.pushNamed(context, LoginForm.routeName);
+                          },
+                          child: Text(
+                            "Login",
+                            style: GoogleFonts.quicksand(
+                              fontSize: 25,
+                              color: const Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(),
+            ],
+          ),
         ),
       ),
     );
